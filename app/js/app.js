744503e41c7583d7c3316f2384d397b3749a12df
cl-dzoba <chris.dzoba@creativelive.com>
2013-06-14 14:06:59 -0700

    End of section 4

--- app/js/app.js
+++ app/js/app.js
@@ -1,15 +1,36 @@
 define([
-  'gapi'
+  'gapi',
+  'views/app',
+  'views/auth',
+  'collections/tasklists'
 ],
 
-function(ApiManager) {
+function(ApiManager, AppView, AuthView, TaskLists) {
   var App = function() {
+    this.views.app = new AppView();
+    this.views.app.render();
+
+    this.views.auth = new AuthView(this);
+    this.views.auth.render();
+
+    this.collections.lists = new TaskLists();
+
     this.connectGapi();
   };
 
   App.prototype = {
+    views: {},
+    collections: {},
     connectGapi: function() {
-      this.apiManager = new ApiManager();
+      var self = this;
+      this.apiManager = new ApiManager(this);
+      this.apiManager.on('ready', function() {
+        self.collections.lists.fetch({ data: { userId: '@me' }, success: function(res) {
+          _.each(res.models, function(model) {
+            console.log(model.get('title'));
+          });
+        }});
+      });
     }
   };
 
